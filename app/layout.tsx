a19e680b605b26991a1d837d4c89b2fecb3c9007
git-for-bit <go.utu98@gmail.com>
2024-10-05 23:08:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Yogita | Personal Portfolio",
+  description: "Yogita Rajput is a Singer/Lyricist/Musician.",
 };
 
 export default function RootLayout({
